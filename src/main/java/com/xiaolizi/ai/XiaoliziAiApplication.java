bc07ed8ccf139f0cb9c01fb0ad28040aca3866f6
lichengwei <lichengwei@tydic.com>
2025-04-28 00:46:15 +0800

    新加聊天接口

---
+++ src/main/java/com/xiaolizi/ai/XiaoliziAiApplication.java
@@ -0,0 +1,13 @@
+package com.xiaolizi.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoliziAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaoliziAiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/xiaolizi/ai/controller/ChatController.java
@@ -0,0 +1,19 @@
+package com.xiaolizi.ai.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/ai")
+public class ChatController {
+    private final ChatClient chatClient;
+    @RequestMapping(value = "/chat",produces = "text/html;charset=UTF-8")
+    public Flux<String> chat(@RequestParam String prompt) {
+        return chatClient.prompt().user(prompt).stream().content();
+    }
+}
