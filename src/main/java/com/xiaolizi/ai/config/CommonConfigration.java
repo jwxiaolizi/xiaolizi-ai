0f573650f1ef9998e652b471fde188e3e8585318
lichengwei <jwxiaolizi@163.com>
2025-04-30 14:55:40 +0800

    聊天功能新加会话日志记录

--- src/main/java/com/xiaolizi/ai/config/CommonConfigration.java
+++ src/main/java/com/xiaolizi/ai/config/CommonConfigration.java
@@ -1,7 +1,10 @@
 package com.xiaolizi.ai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,11 +12,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CommonConfigration {
     @Bean
-    public ChatClient chatClient(OllamaChatModel ollamaChatModel) {
+    public ChatMemory chatMemory() {
+        return new InMemoryChatMemory();
+    }
+    @Bean
+    public ChatClient chatClient(OllamaChatModel ollamaChatModel, ChatMemory chatMemory) {
         return ChatClient
                 .builder(ollamaChatModel)
                 .defaultSystem("你是一个ai智能助手，请用林黛玉的口吻回答用户的问题")
-                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .defaultAdvisors(new SimpleLoggerAdvisor(),
+                        new MessageChatMemoryAdvisor(chatMemory))
                 .build();
     }
 }
--- src/main/java/com/xiaolizi/ai/controller/ChatController.java
+++ src/main/java/com/xiaolizi/ai/controller/ChatController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
+
 /**
  * @author l4784
  */
@@ -16,9 +18,11 @@ import reactor.core.publisher.Flux;
 public class ChatController {
     private final ChatClient chatClient;
     @RequestMapping(value = "/chat",produces = "text/html;charset=UTF-8")
-    public Flux<String> chat(@RequestParam String prompt) {
+    public Flux<String> chat(@RequestParam String prompt,
+                             @RequestParam String chatId) {
         return chatClient.prompt()
                             .user(prompt)
+                            .advisors(advisors -> advisors.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId))
                             .stream()
                             .content();
     }
